331a901f947cdce6f1296b2f93c614610f8dbb18
Dusan Uradnik <dusanuradnik23@gmail.com>
2022-06-20 22:52:18 +0200

    Add boxplot to systemtap's evaluator

--- systemtap/evaluator.py
+++ systemtap/evaluator.py
@@ -27,10 +27,11 @@ def check_parent_dir(arg: str):
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-p', '--providers', nargs='+', default=providers)
-parser.add_argument('-f', '--files', type=list,
+parser.add_argument('-f', '--files', nargs='+', type=str,
                     default=['csv/stap.csv', 'csv/old_stap.csv'])
 parser.add_argument('-o', '--output', type=lambda x: check_parent_dir(x),
                     default='figures/figure.png')
+parser.add_argument('--scatterplot', action='store_true')
 args = parser.parse_args()
 
 
@@ -62,36 +63,50 @@ plt.rc('legend', loc="upper right")
 my_palette = ['#fb4d3d', '#345995']
 another_palette = ['#000000', '#fcba03']
 sns.set_palette(palette=my_palette)
-fig, axes = plt.subplots(len(col_names), 1, figsize=(18/2.54, 3*(28/2.54)))
 
-custom_legend = [Line2D([0], [0], color='black', label='Mean'),
-                 Line2D([0], [0], color='black', label='Median', linestyle='--'),
-                 Line2D([0], [0], color='black', label='Q1', linestyle=':'),
-                 Line2D([0], [0], color='black', label='Q3', linestyle='-.')]
 
-for ax, name in zip(axes, col_names):
-    ax.set_xlabel('Num. of request')
-    ax.set_ylabel('Time (us)')
-    ax.title.set_text(name)
+if args.scatterplot:
+    custom_legend = [Line2D([0], [0], color='black', label='Mean'),
+                     Line2D([0], [0], color='black', label='Median', linestyle='--'),
+                     Line2D([0], [0], color='black', label='Q1', linestyle=':'),
+                     Line2D([0], [0], color='black', label='Q3', linestyle='-.')]
 
-    ax.grid(axis='y', alpha=0.8, linewidth=0.5)
+    fig, axes = plt.subplots(len(col_names), 1, figsize=(18/2.54, 3*(28/2.54)))
 
-    curr = data.loc[data.provider == name].reset_index()
-    sns.scatterplot(ax=ax, data=curr, x=curr.req_num, y=curr.time, hue=curr.version, s=8)
-    handles, labels = ax.get_legend_handles_labels()
-    for i, vrsn in enumerate(curr.version.unique()):
-        sep_vrsn = curr.loc[curr.version == vrsn]
-        ax.axhline(sep_vrsn.time.mean(), color=my_palette[i])
-        ax.axhline(sep_vrsn.time.median(), color=my_palette[i], linestyle='--')
-        ax.axhline(sep_vrsn.time.quantile(0.25, interpolation='midpoint'),
-                   color=my_palette[i], linestyle=':')
-        ax.axhline(sep_vrsn.time.quantile(0.75, interpolation='midpoint'),
-                   color=my_palette[i], linestyle='-.')
+    for ax, name in zip(axes, col_names):
+        ax.set_xlabel('Num. of request')
+        ax.set_ylabel('Time (us)')
+        ax.title.set_text(name)
 
-    ax.add_artist(ax.legend(handles, labels, loc='upper right'))
-    ax.legend(handles=custom_legend, loc='upper left')
+        ax.grid(axis='y', alpha=0.8, linewidth=0.5)
+
+        curr = data.loc[data.provider == name].reset_index()
+        sns.scatterplot(ax=ax, data=curr, x=curr.req_num, y=curr.time, hue=curr.version, s=8)
+        handles, labels = ax.get_legend_handles_labels()
+        for i, vrsn in enumerate(curr.version.unique()):
+            sep_vrsn = curr.loc[curr.version == vrsn]
+            ax.axhline(sep_vrsn.time.mean(), color=my_palette[i])
+            ax.axhline(sep_vrsn.time.median(), color=my_palette[i], linestyle='--')
+            ax.axhline(sep_vrsn.time.quantile(0.25, interpolation='midpoint'),
+                       color=my_palette[i], linestyle=':')
+            ax.axhline(sep_vrsn.time.quantile(0.75, interpolation='midpoint'),
+                       color=my_palette[i], linestyle='-.')
+
+        ax.add_artist(ax.legend(handles, labels, loc='upper right'))
+        ax.legend(handles=custom_legend, loc='upper left')
+else:
+    fig, ax = plt.subplots(1, 1, figsize=(18/2.54, 28/2.54))
+
+    boxplot = sns.boxplot(ax=ax, data=data, x='provider', y='time', hue='version')
+
+    boxplot.set_xlabel('num. of request')
+    boxplot.set_ylabel('time (us)')
+    boxplot.grid(axis='y', alpha=0.8, linewidth=0.5)
+
 
 plt.tight_layout()
 
-plt.savefig(args.output)
+if args.output:
+    plt.savefig(args.output)
+
 plt.show()
